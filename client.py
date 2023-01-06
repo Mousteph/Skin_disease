d71904bfd322cf6e6d44170498bfced5d2c9e83f
Moustapha DIOP <moustephdiop@gmail.com>
2023-01-06 18:22:30 +0100

    (update) cli

--- client.py
+++ client.py
@@ -1,50 +1,50 @@
-import requests
 import base64
-import json
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+from src.sender import send_image
 
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 url = "http://127.0.0.1:8089/predict"
 
 def transform_image(img_path):
-    with open(img_path, "rb") as f:
-        im_bytes = f.read()
-
-    im_b64 = base64.b64encode(im_bytes).decode("utf8")
-
-    return im_b64
-
-def send_image(img, url, explain):
-    payload = json.dumps({"image": img, "explain": explain})
-    response = requests.post(url, data=payload, headers=headers)
-
     try:
-        data = response.json()
+        with open(img_path, "rb") as f:
+            im_bytes = f.read()
+    except Exception:
+        print("Cannot open image: " + img_path)
+        exit(1)
+
+    return base64.b64encode(im_bytes).decode("utf8")
+
+def get_prediction(img, url, explain, precision):
+    data = send_image(img, url, explain, precision)
+    
+    if not data.get('success'):
+        print("Error: " + data.get('error'))
+    else:
+        prediction = data.get('prediction')
+        probability = data.get('probability')
         explain = data.get('image')
         
-        print(f"prediction: {data.get('prediction')} - {data.get('probability')}")
+        print(f"prediction: {prediction} - {round(probability, 2) * 100}%")
         
         if explain is not None:
             plt.imshow(np.array(explain))
             plt.show()
 
-    except requests.exceptions.RequestException:
-        print(response.text)
-
-
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--explain", action="store_true", help="Have an explanation")
-    parser.add_argument("image", nargs='+', help='path to the image')
+    parser.add_argument("--explain", action="store_true", help="If the server should explain the prediction")
+    parser.add_argument("--precision", default="Moyenne", help="Precision of the explanation (Faible, Moyenne, Importante)")
+    parser.add_argument("image", nargs=1, help='path to the image')
     args = parser.parse_args()
 
     if len(sys.argv) < 2:
         exit(0)
-
+        
     img = transform_image(args.image[0])
-    send_image(img, url, args.explain)
+    get_prediction(img, url, args.explain, args.precision)
 
